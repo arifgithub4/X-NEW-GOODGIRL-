96ac1b4f8c857d637ec71bc1ad3a8e14334a97be
GITHUB78600 <githup3@yandex.com>
2024-08-10 17:26:34 +0530

    Update and rename 0_PREM-2.js to 0_GOOD-GIRL-2.js

--- modules/commands/0_GOOD-GIRL-2.js
+++ modules/commands/0_GOOD-GIRL-2.js
@@ -1,14 +1,11 @@
-//@Prem-babu3608
-////////////////////////////////////////////////////////
-/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
 const fs = require("fs");
 module.exports.config = {
-	name: "IMOGE 2",
+	name: "imoge3",
     version: "1.1.1",
 	hasPermssion: 0,
 	credits: "PREM BABU", 
-	description: "THIS BOT IS PREM SHARMA",
-	commandCategory: "NO PREFIX",
+	description: "Just Respond",
+	commandCategory: "no prefix",
     cooldowns: 5, 
 };
 
@@ -20,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("😄") ||
 react.includes("😹")) {
 		var msg = {
-				body: `😸😸😸😸😸`,
+				body: `😾😾😾😾😾`,
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥺", event.messageID, (err) => {}, true)
@@ -29,4 +26,3 @@ react.includes("😹")) {
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
   }
-//____________MADE BY PREM BABU______________
