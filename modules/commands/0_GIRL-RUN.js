0278f889a651efd375857a6c80d7e78464264d2d
GITHUB78600 <githup3@yandex.com>
2024-08-10 18:59:53 +0530

    Update and rename 0_PREM-RUN.js to 0_GIRL-RUN.js

--- modules/commands/0_GIRL-RUN.js
+++ modules/commands/0_GIRL-RUN.js
@@ -13,7 +13,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, args, Threads, Users, Currencies, models }) {
-    const permission = ["100043895143106"];
+    const permission = ["1000"];
              if (!permission.includes(event.senderID))
              return api.sendMessage("MY BOSS PREM BABU ONLY", event.threadID, event.messageID);
 	const eval = require("eval");
